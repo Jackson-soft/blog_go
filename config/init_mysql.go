e34bcd5071e2011276e7a4871be0aba4e40b9e71
Jackson-soft <wxw198812@gmail.com>
2016-06-27 22:55:06 +0800

    mysql table

--- config/init_mysql.go
+++ config/init_mysql.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
 )
--- sql/demo.sql
+++ sql/demo.sql
@@ -1,1 +1,7 @@
-create table `
+create table `user_info`
+(
+    `id` bigint(20) unsigned not null auto_increment comment '主键',
+    `mobile` varchar(11) not null comment '手机号',
+    `password` varchar(128) not null comment '密码',
+    primary key(`id`)
+)engine=InnoDB default charset=utf8 comment='用户表';
