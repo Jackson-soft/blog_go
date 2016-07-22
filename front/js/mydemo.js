bd7527e19c6694b8d9942477c6d691e131cd44e9
jinfo <jinfo@jiayukang.com>
2016-07-22 17:38:07 +0800

    add js file

--- front/js/mydemo.js
+++ front/js/mydemo.js
@@ -1,4 +1,13 @@
-function UserLogin()
-{
-    htmlobj = $.post(/user)
+var URL = "http://127.0.0.1:8080"
+function UserLogin() {
+    $.ajax({
+        url: URL + "/userlogin/login",
+        type: "POST",
+        data: {
+            mobile: "12345678901",
+            password: "123456"
+        }
+    }).done(function(data){
+        alert(data);
+    })
 }
